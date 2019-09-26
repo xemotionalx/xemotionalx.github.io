d0b454f6db7a37b1a04675dfb50a42db6a7ad2b3
maiada <maiadacx@gmail.com>
2019-09-26 16:00:58 -0400

    added animations to picture frames in about me section but 2 of them do not expand correctly. changed the navbar to have a transparent bg and shadow text - has an issue with popping up too soon when scrolling down from home

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,64 @@
 document.addEventListener('DOMContentLoaded', function() {
-window.onscroll = function() {myFunction()};
 
+/** NAVBAR **/
+
+window.onscroll = function() {navFunction()};
 // Get the navbar
 var navbar = document.getElementById("navbar");
-
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
-
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function navFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
 }
+
 });
+
+/** ABOUT ME FRAME POP UPS **/
+
+var imgClicked = false;
+
+function imgOnClick() {
+
+  var number = $(this).attr("value");
+
+  if (imgClicked) {
+
+    $(this).animate({
+      width: '100%',
+    });
+
+    $(this).css("z-index", "2");
+    
+    $(".framed-text--" + number).css("display", "none");
+    imgClicked = false;
+
+  } else {
+
+    if (number === 2 || number === 4) {
+      $(this).animate({
+        width: '100px',
+        height: 'auto'
+      });
+    } else {
+    $(this).animate({
+      width: '200%',
+    });
+  }
+
+  $(this).css("z-index", "98");
+
+  $(".framed-text--" + number).css("display", "block");
+  imgClicked = true;
+  }
+
+};
+
+
+$(document).on("click", ".framed-img", imgOnClick);
+
+
