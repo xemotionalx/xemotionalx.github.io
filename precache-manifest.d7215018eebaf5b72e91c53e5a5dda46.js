2914725e8617c70fee4dc389afaa14df6fe304e5
maiada <maiadacx@gmail.com>
2020-02-13 00:48:37 -0500

    Updates

---
+++ precache-manifest.d7215018eebaf5b72e91c53e5a5dda46.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df384f465ea324d00956c652088c8789",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79375fb855c7bcfebb5a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ab1b5379e77ebac374a7",
+    "url": "/static/css/main.226326a2.chunk.css"
+  },
+  {
+    "revision": "79375fb855c7bcfebb5a",
+    "url": "/static/js/2.d09b558a.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.d09b558a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1b5379e77ebac374a7",
+    "url": "/static/js/main.7db2884a.chunk.js"
+  },
+  {
+    "revision": "d0510992af86046b892f",
+    "url": "/static/js/runtime-main.d87c0e5b.js"
+  },
+  {
+    "revision": "1431a2e2cca2450d9114daa10c2a7e32",
+    "url": "/static/media/MaiAda-Carpano-Resume.1431a2e2.pdf"
+  },
+  {
+    "revision": "e4538aa6e34821ae7836ee54a3eef9cc",
+    "url": "/static/media/bestreads.e4538aa6.png"
+  },
+  {
+    "revision": "53d18a1ef58e092f5d2b17d87df1b761",
+    "url": "/static/media/datenight.53d18a1e.png"
+  },
+  {
+    "revision": "161af8078f564cc35002dfdd2f9f13aa",
+    "url": "/static/media/pairigy.161af807.png"
+  },
+  {
+    "revision": "c0be0dc51fc5159721558a5f5c3650ba",
+    "url": "/static/media/portfolio-icon-1.c0be0dc5.png"
+  },
+  {
+    "revision": "cef3b21e0606bc4119c160405a042abe",
+    "url": "/static/media/portfolio-icon-10.cef3b21e.png"
+  },
+  {
+    "revision": "afc69fd3eb69bbe242b2f27a9d7906a0",
+    "url": "/static/media/portfolio-icon-11.afc69fd3.png"
+  },
+  {
+    "revision": "e36cdd1612678f8e955499c05bee88de",
+    "url": "/static/media/portfolio-icon-2.e36cdd16.png"
+  },
+  {
+    "revision": "fe1dea5d712437df7016a0442034d642",
+    "url": "/static/media/portfolio-icon-3.fe1dea5d.png"
+  },
+  {
+    "revision": "0ea598b2d8bd2356da0ff66387207d66",
+    "url": "/static/media/portfolio-icon-4.0ea598b2.png"
+  },
+  {
+    "revision": "d0f5e29259d1748bd4045efaf4f04a6c",
+    "url": "/static/media/portfolio-icon-5.d0f5e292.png"
+  },
+  {
+    "revision": "54937eaf651757ee53e1abb3bf42eb28",
+    "url": "/static/media/portfolio-icon-6.54937eaf.png"
+  },
+  {
+    "revision": "95035a4dc55364440de9663360289f1a",
+    "url": "/static/media/portfolio-icon-7.95035a4d.png"
+  },
+  {
+    "revision": "a121a7513037bb12cbda770e61c33854",
+    "url": "/static/media/portfolio-screenie-bestreads.a121a751.png"
+  },
+  {
+    "revision": "58854299bc5de94626f96d4a2877ca15",
+    "url": "/static/media/portfolio-screenie-datenight.58854299.png"
+  },
+  {
+    "revision": "e2198b7e2f7556ed2fec45461e7772d3",
+    "url": "/static/media/portfolio-screenie-pairigy.e2198b7e.png"
+  },
+  {
+    "revision": "b2722fa6fd3350e3f12bb01111345f46",
+    "url": "/static/media/selfportrait.b2722fa6.png"
+  }
+]);
