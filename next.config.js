3fc3e13aab8f040ec0dbcef5c3b8e5a678ebc3c3
MaiAda Carpano <52050339+xemotionalx@users.noreply.github.com>
2021-10-02 22:42:15 -0700

    added config for url-loader

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 module.exports = {
   reactStrictMode: true,
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
+    // config.module.rules.push({
+    //   test: /\.svg$/,
+    //   use: ["@svgr/webpack"]
+    // });
+    config.module.rules.push(
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000' 
+      }
+    )
     return config;
   }
 }
